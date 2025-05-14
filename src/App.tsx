065d270c3bccc6867bcceda6677cd422b8a80cab
Fjmarz <facumarzetti@gmail.com>
2025-05-14 11:27:10 +0200

    Remove unused components and update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
-import AthleteLogin from './components/AthleteLogin';
-import CoachLogin from './components/CoachLogin';
 import Login from './components/Login';
 
 function App() {
@@ -10,8 +8,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/atleta" element={<AthleteLogin />} />
-        <Route path="/coach" element={<CoachLogin />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
