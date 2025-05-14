776f75c32bdbacedeef0665f3efc923ba561509e
Fjmarz <facumarzetti@gmail.com>
2025-05-14 11:15:08 +0200

    Create login page with email/password and Google auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import AthleteLogin from './components/AthleteLogin';
 import CoachLogin from './components/CoachLogin';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/atleta" element={<AthleteLogin />} />
         <Route path="/coach" element={<CoachLogin />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
