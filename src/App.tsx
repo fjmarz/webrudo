9b8eacae5fe43c4c53c314a19753833c59e73641
Fjmarz <facumarzetti@gmail.com>
2025-05-15 15:39:38 +0200

    Fix registration route and component imports

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/signup" element={<Register />} /> {/* Adding an alias route */}
     </Routes>
   );
 }
